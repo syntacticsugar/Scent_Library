b28b4d27ee5fbf4ceafb43e8e77bcb737a919cb4
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2013-11-18 23:37:14 +0000

    Corrected timestamps.

--- model.rb
+++ model.rb
@@ -1,5 +1,5 @@
 require 'data_mapper'
-#require 'dm-sqlite-adapter'
+require 'dm-timestamps'
 
 DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_COPPER_URL'] || "sqlite3://#{Dir.pwd}/development.db")
 
@@ -10,8 +10,7 @@ class Juice
   property :id,           Serial
   property :brand,        String, :required => true
   property :name,         String, :required => true
-  property :used,         DateTime
-  property :added,        DateTime
+  property :created_at, DateTime # Magic property will be set automatically.
   property :size,         String
   property :formula,      String
   property :description,  String
@@ -43,7 +42,7 @@ class PersonJuice
 
   property :owned, Boolean, :default => false
   property :wished_for, Boolean, :default => false
-  
+  property :used_at, DateTime
   property :rating, Integer # 0 to 10
 end
 
